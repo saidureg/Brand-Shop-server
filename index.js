58e79660bcb767f367c8c10a942dc7ab197d0caf
saidureg <saidureg@gmail.com>
2023-10-18 23:58:55 +0600

    post method is implement for product add

--- index.js
+++ index.js
@@ -23,6 +23,15 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const productsCollection = client.db("brandShopDB").collection("products");
+
+    app.post("/addProduct", async (req, res) => {
+      const newProduct = req.body;
+      const result = await productsCollection.insertOne(newProduct);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
